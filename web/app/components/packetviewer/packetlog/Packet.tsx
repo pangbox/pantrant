8e4edf7f8ef1910c1591077909237ca0ac7d3211
Eric Chiz <eric.chiz@gmail.com>
2022-02-23 15:51:25 -0500

    Minor improvements (#21)
    * Add Packetdoc link to Packet Viewer
    
    * Fix link for Hello packets
    
    * Update packet names to reflect Packetdoc
    
    Packets that are still entirely undocumented in Packetdoc are still labelled with the hsreina names, prefixed with 'UNDOC-HS-'. Packets that have a stub entry but no concrete information/name are labeled 'UNDOC-STUB'.
    
    * Recompile
    
    Contains new packet names and Packetdoc button
    
    * Fix sort for packet bursts
    
    Packets that appear in bursts / have the same timestamp weren't sorted into the correct order; now, if multiple packets have the same timestamp, the internal sequence ID Pantrant assigns is used as well.
    
    * Fix typo in GSS-021E
    
    Achievemtn -> Achievement
    
    * Make timestamp and sequence ID visible
    
    Makes visible the video timestamp and internal Pantrant sequence ID for each packet, which may be of some help to users. Making sequence ID visible helped to solve sort bug fixed in previous commit.
    
    * Recompile
    
    Recompiled with all modifications from 2022-02-22.

--- web/app/components/packetviewer/packetlog/Packet.tsx
+++ web/app/components/packetviewer/packetlog/Packet.tsx
@@ -44,20 +44,33 @@ export const Packet = ({ event }: Props) => {
       <>&larr; {event.ServerKind}</>
     );
 
+  let packetOrigin = {
+    "gameserver": "gameservice",
+    "loginserver": "loginservice",
+    "messageserver": "messageservice"
+  }
+  let packetData = new Uint16Array(event.Message.Data.slice(0, 2));
+  let packetDocURL = (event.Type === "Connect") ? "https://packets.pangdox.com/protocol.html" : "https://packets.pangdox.com/packets/" + packetOrigin[event.ServerKind.toLowerCase()] + "/" + event.Message.Origin.toLowerCase() + "/" + ("0000" + packetData[0].toString(16)).substr(-4)
+
   const controls = 
     <VideoConsumer>
       {videoControl =>
         <ButtonGroup>
           <Button minimal={true} small={true} icon="cross" onClick={() => setCollapsed(true)}>Close</Button>
           <Button minimal={true} small={true} icon="link" onClick={() => videoControl.currentTime = event.Time}>Seek to</Button>
+          <Button minimal={true} small={true} icon="link" onClick={() => window.open(packetDocURL, "_blank")}>Packetdoc</Button>
         </ButtonGroup>
       }
     </VideoConsumer>;
+  
+  let packetTimestamp = ( (event.Time >= 3600) ? Math.floor(event.Time/3600) + ":" : "" ) +
+                        ( (event.Time >= 60) ? Math.floor((event.Time % 3600)/60) + ":" : "" ) + 
+                        Math.floor(event.Time % 60) + "." + Math.floor(event.Time * 1000 % 1000);
 
   return (
     <Card interactive={collapsed} className={classes.join(" ")} onClick={() => collapsed ? setCollapsed(false) : null}>
       {collapsed ? null : <div>{controls}<hr/></div>}
-      <div><small>{direction} &bull; {name}</small></div>
+      <div><small>{direction} &bull; {packetTimestamp} &bull; Seq# {event.ID} &bull; {name} </small></div>
       <HexView data={event.Message.Data} collapsed={collapsed}></HexView>
     </Card>
   );
